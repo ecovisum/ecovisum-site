5f2a1ce12dc3039fa041efe116fd669abae99204
Dave Everitt <deveritt@innotts.co.uk>
2019-04-12 13:00:41 +0100

    working on multiple Markdwon files, but ugly code

--- js/mdconvert.js
+++ js/mdconvert.js
@@ -1,31 +1,45 @@
 const page1 = document.getElementById('page-1');
+const page2 = document.getElementById('page-2');
+const page3 = document.getElementById('page-3');
+const page4 = document.getElementById('page-4');
 
 const converter = new showdown.Converter();
-let rawMd;
+let page;
 
-const getMarkdown = (filename) => {
-  fetch(filename, {credentials: 'same-origin'})
-    .then((response) => response.text())
-    .then(convertMd)
+const getMarkdown = (filename, mdPage) => {
+  fetch(filename, mdPage, { credentials: 'same-origin' })
+    .then((response) => {
+      // console.log(mdPage, response);
+      response.text().then(function (text) {
+        let mdText = text;
+        convertedText = converter.makeHtml(mdText);
+        mdPage.innerHTML = convertedText;
+      })
+    })
     .catch(e => console.log("Something's wrong…"));
-  // return true; //no difference
 }
 
-const convertMd = (retrievedText) => {
-  convertedText = converter.makeHtml(retrievedText);
-  page1.innerHTML = convertedText;
-}
+// const convertMd = (md) => {
+//   console.log(md);
+//   convertedText = converter.makeHtml(md);
+//   page.innerHTML = convertedText;
+// }
 
-const fileName = './markdown/index.md';
-getMarkdown(fileName);
+// let fileName = './markdown/index.md';
+getMarkdown('./markdown/index.md', page1);
+// fileName = './markdown/problem.md';
+getMarkdown('./markdown/problem.md', page2);
 
 
 
 // ========= ORIGINAL EXAMPLE =========
-// fetch('./index.md', {credentials: 'same-origin'})
+  // fetch(filename, {credentials: 'same-origin', mode: 'no-cors'})
+
 // const convertmd = (md) => {
 //   const converter = new showdown.Converter(),
 //   text = '# hello, markdown!',
 //   html = converter.makeHtml(text);
 //   console.log(html);
 // }
+
+  // return true; //no effect
--- readme.md
+++ readme.md
@@ -1,11 +1,19 @@
 # Website for Ecovisum
 
-See [draft website text](ecovisum.md)—under review, pending arrangement into sections/pages.
+See [draft website text](markdown/ecovisum.md)—under review, pending arrangement into sections/pages.
 
 ## TO DO
 
-- [ ] rewrite content (this is a straight copy)
-- [ ] fix contact form
-- [ ] style it as wireframe
 - [ ] do mobile menu
+- [ ] make Markdown converter call map through files
+- [ ] style it as wireframe
 - [ ] make design as a skin
+- [x] make Markdown converter generic
+- [x] rewrite content (this is a straight copy)
+- [x] decide on whether we need a contact form (not yet)
+
+## References
+
+- [answer to "Get text from a txt file in the url"](https://stackoverflow.com/a/39758157/123033)
+- [Showdown.js](http://showdownjs.com/)
+- [Jake Archibald: "That's so fetch!"](https://jakearchibald.com/2015/thats-so-fetch/)
