317cc86c150daadac99f49eca26e9772f18b74de
Dave Everitt <deveritt@innotts.co.uk>
2019-05-13 14:42:39 +0100

    added costing tiers data

---
+++ js/tiers.js
@@ -0,0 +1,32 @@
+const tiers = [
+  {
+    Tier: "Basic",
+    Buildings: 5,
+    Price: 500,
+    Extra: 100
+  },
+  {
+    Tier: "Small",
+    Buildings: 10,
+    Price: 1000,
+    Extra: 50
+  },
+  {
+    Tier: "Medium",
+    Buildings: 3000,
+    Price: 3000,
+    Extra: 20
+  },
+  {
+    Tier: "Large",
+    Buildings: 150,
+    Price: 5000,
+    Extra: 10
+  },
+  {
+    Tier: "X-large",
+    Buildings: 500,
+    Price: 8500,
+    Extra: 5
+  },
+]
